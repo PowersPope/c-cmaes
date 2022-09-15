1ee66f6fa113449eadf0f24208de4df65cc0970e
Andrew Powers <apowers4@uoregon.edu>
2022-09-15 14:59:23 -0700

    Configured the header random_noise file, so that it includes the random matrix geneartion function.

--- src/random_noise.h
+++ src/random_noise.h
@@ -13,15 +13,13 @@ std::vector<std::vector<double>> generateNoiseMatrix (int N; double mu; double s
     std::normal_distribution<double> distribution(mu, simga); // Specify Normal Distribution
 
     std::vector<std::vector<double>> mat(N, vector<double> (N,0));
-    for (int i = 0; i < N; i++)
-    {
-        for(int j = 0; j < m; j++)
-        {
-            std::cout << mat[i][j] << '\t';
-        }
-        std::cout << std::endl;
-    }
 
+    // Iter through mat element by element to add random noise to the matrix
+    for (int i = 0; i < N; ++i){
+        for (int j = 0; j < N; ++j){
+            mat[i][j] = distribution(generator);
+        }
+    }
 
     return mat;
 }
